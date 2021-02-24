46721283db161f25db8cd8da6852063b53e73945
Jan Nielsen <jan.sture.nielsen@gmail.com>
2021-02-24 12:33:48 -0700

    Polish -- tabs to spaces.

--- src/main/java/so/downloader/Application.java
+++ src/main/java/so/downloader/Application.java
@@ -11,24 +11,24 @@ import java.nio.file.Paths;
  * https://stackoverflow.com/questions/921262/how-to-download-and-save-a-file-from-internet-using-java/47598360#47598360
  */
 public class Application {
-	public static void main(String[] args) throws MalformedURLException, IOException {
-		if (2 != args.length) {
-			System.out.println(String.format("USAGE: java -jar so-downloader.jar <source-URL> <target-filename>"));
-			System.exit(1);
-		}
+    public static void main(String[] args) throws MalformedURLException, IOException {
+        if (2 != args.length) {
+            System.out.println(String.format("USAGE: java -jar so-downloader.jar <source-URL> <target-filename>"));
+            System.exit(1);
+        }
 
-		String sourceUrl = args[0];
-		String targetFilename = args[1];
+        String sourceUrl = args[0];
+        String targetFilename = args[1];
 
-		long bytesDownloaded = download(sourceUrl, targetFilename);
-		System.out.println(String.format("Downloaded %d bytes from %s to %s.", bytesDownloaded, sourceUrl, targetFilename));
-	}
+        long bytesDownloaded = download(sourceUrl, targetFilename);
+        System.out.println(String.format("Downloaded %d bytes from %s to %s.", bytesDownloaded, sourceUrl, targetFilename));
+    }
 
-	// package scope to allow test class to access
-	static long download(String sourceUrl, String targetFileName) throws MalformedURLException, IOException {
-	    try (InputStream in = URI.create(sourceUrl).toURL().openStream()) {
-	        return Files.copy(in, Paths.get(targetFileName));
-	    }
-	}
+    // package scope to allow test class to access
+    static long download(String sourceUrl, String targetFileName) throws MalformedURLException, IOException {
+        try (InputStream in = URI.create(sourceUrl).toURL().openStream()) {
+            return Files.copy(in, Paths.get(targetFileName));
+        }
+    }
 
 }
--- src/test/java/so/downloader/ApplicationTest.java
+++ src/test/java/so/downloader/ApplicationTest.java
@@ -10,24 +10,24 @@ import java.net.MalformedURLException;
 import org.junit.jupiter.api.Test;
 
 public class ApplicationTest {
-	@Test
-	public void downloadFile() throws MalformedURLException, IOException {
-		assertThat(Application.download("https://github.com/JanStureNielsen/so-downloader/archive/main.zip", "so-downloader-source.zip")).isNotZero();
-	}
+    @Test
+    public void downloadFile() throws MalformedURLException, IOException {
+        assertThat(Application.download("https://github.com/JanStureNielsen/so-downloader/archive/main.zip", "so-downloader-source.zip")).isNotZero();
+    }
 
-	@Test
-	public void illegalArgument() throws MalformedURLException, IOException {
-		assertThrows(IllegalArgumentException.class, () -> Application.download("this-is-not-a-url", "some-file"));
-	}
+    @Test
+    public void illegalArgument() throws MalformedURLException, IOException {
+        assertThrows(IllegalArgumentException.class, () -> Application.download("this-is-not-a-url", "some-file"));
+    }
 
-	@Test
-	public void fileNotFound() throws MalformedURLException, IOException {
-		assertThrows(FileNotFoundException.class, () -> Application.download("file:///this-is-not-found", "some-file"));
-	}
+    @Test
+    public void fileNotFound() throws MalformedURLException, IOException {
+        assertThrows(FileNotFoundException.class, () -> Application.download("file:///this-is-not-found", "some-file"));
+    }
 
-	@Test
-	public void malformedUrl() throws MalformedURLException, IOException {
-		assertThrows(MalformedURLException.class, () -> Application.download("protocol-invalid:///this-is-not-found", "some-file"));
-	}
+    @Test
+    public void malformedUrl() throws MalformedURLException, IOException {
+        assertThrows(MalformedURLException.class, () -> Application.download("protocol-invalid:///this-is-not-found", "some-file"));
+    }
 
 }
